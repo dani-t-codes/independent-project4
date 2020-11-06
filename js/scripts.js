b73ea46351df867293e9606806f01a8c4675641d
Danielle Thompson <danithompson74@gmail.com>
2020-11-06 11:23:14 -0800

    Fix form so all sections submit

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,33 @@ $(document).ready(function() {
   $("form#pizza-builder").submit(function(event) {
     event.preventDefault();
     $("#dream-pizza-show").show();
+//    let userResponses = [];
     $("input:checkbox[name=pizza-size]:checked").each(function() {
       const pizzaSize = $(this).val();
-      $('#dream-pizza-show').append(pizzaSize + "<br>");
+      $('#pizza-size').append(pizzaSize + "<br>");
+//      userResponses.push(pizzaSize);
+    });
+    $("input:checkbox[name=pizza-crust]:checked").each(function() {
+      const pizzaCrust = $(this).val();
+      $('#pizza-crust').append(pizzaCrust + "<br>");
+    });
+    $("input:checkbox[name=pizza-cheese]:checked").each(function() {
+      const pizzaCheese = $(this).val();
+      $('#pizza-cheese').append(pizzaCheese + "<br>");
+    });
+    $("input:checkbox[name=pizza-sauce]:checked").each(function() {
+      const pizzaSauce = $(this).val();
+      $('#pizza-sauce').append(pizzaSauce + "<br>");
+    });
+    $("input:checkbox[name=pizza-meat]:checked").each(function() {
+      const pizzaMeat = $(this).val();
+      $('#pizza-meat').append(pizzaMeat + "<br>");
+    });
+    $("input:checkbox[name=pizza-veg]:checked").each(function() {
+      const pizzaVeg = $(this).val();
+      $('#pizza-veg').append(pizzaVeg + "<br>");
     });
     $('#pizza-builder').hide();
+    $('#order-instructions').hide();
   });
 });
