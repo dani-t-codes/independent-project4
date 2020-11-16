9c0218717196a5b7ba6e84778b6fd45b6bd7b195
Danielle Thompson <danithompson74@gmail.com>
2020-11-16 00:15:04 -0800

    Add notes on non-returns, create global pizzaRolodex, update UI fxns

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ function DreamPizza(size, crust, sauce, meatToppings, vegToppings, totalCost) {
 
 DreamPizza.prototype.fullPizzaDetails = function() {
   return this.size + ' pizza with ' + this.crust + " crust, " + this.sauce + ' sauce, ' + this.meatToppings + ", and " + this.vegToppings + ".";
-}
+} //currently returning undefined pizze with undefined crust ... etc. 
 
 DreamPizza.prototype.sizeCost = function() {
   if (this.size === 'Medium') {
@@ -33,7 +33,7 @@ DreamPizza.prototype.sizeCost = function() {
   return this.totalCost;
 };
 
-DreamPizza.prototype.addOnGFCost = function() {
+DreamPizza.prototype.addOnGFCost = function() { // could any of these multiple conditional statements get switched to "switch/case" statements? 
   if (this.crust === 'Gluten-Free') {
     this.totalCost += 5;
   }
@@ -55,36 +55,41 @@ DreamPizza.prototype.addOnVegToppingsCost = function() {
 };
 
 //User Interface ------------
-let dreamPizza = new DreamPizza();
+let pizzaRolodex = new PizzaRolodex();
 
 function displayCostDetails(dreamPizzaCostToDisplay) { 
   let pizzaCostDisplay = $("ul#pizza-cost");
   let htmlForPizzaCost = '';
-  dreamPizzaCostToDisplay.dreamPizza.forEach(function(DreamPizza) {
+  dreamPizzaCostToDisplay.pizzas.forEach(function(dreamPizza) {
     htmlForPizzaCost += DreamPizza.prototype.sizeCost;
     DreamPizza.prototype.addOnGFCost;
     DreamPizza.prototype.addOnMeatToppingsCost;
     DreamPizza.prototype.addOnVegToppingsCost;
-    pizzaCostDisplay.html(htmlForPizzaCost); //currently returns NaN
   });
-  console.log(displayCostDetails(dreamPizzaCostToDisplay));
+  pizzaCostDisplay.html(htmlForPizzaCost); //currently not returning at all
 }
 // dreamPizza.html(this.totalCost.sizeCost.addOnGFCost.addOnToppingsCost); //as is does not work to combine all the fxns
 
 function displayPizzaDetails(dreamPizzaDisplay) {
   let pizzaBuildDisplay = $("ul#pizza-build");
   let htmlForPizzaDeets = '';
-  dreamPizzaDisplay.dreamPizza.forEach(function(DreamPizza) {
+  dreamPizzaDisplay.pizzas.forEach(function(dreamPizza) { //error message "Uncaught typeError: cannot read property 'forEach' of undefined"
     htmlForPizzaDeets += "<li>" + DreamPizza.size + ' pizza with ' + DreamPizza.crust + " crust, " + DreamPizza.sauce + ' sauce, ' + DreamPizza.meatToppings + ", " + DreamPizza.vegToppings + "." + "</li>";
   });
   pizzaBuildDisplay.html(htmlForPizzaDeets);
 }
-// let userDreamPizza = $("TODO");
-// let htmlForPizzaDeets = '';
-// displayPizzaDetails.forEach(function(userDreamPizza) {
-//   userDreamPizza.concat(userDreamPizza.size + " pizza with " + DreamPizza.crust + " crust, " + DreamPizza.sauce + " sauce, " + DreamPizza.cheese + ", and " + DreamPizza.toppings); //lsn23 cheat sheet
-// });
-// userDreamPizza.html(htmlForPizzaDeets);
+
+function showPizza(pizzas) {
+  const pizza = addressBook(pizza[0]); //might not work - need a fxn
+  $("#dream-pizza-show").show();
+  $("#pizza-build").html(dreamPizza.size); //newDreamPizza needs to be DP keys ... probably individually added
+}
+
+function attachPizzaListeners() {
+  $("ul#pizza-build").on("click", "li", function() {
+    showPizza(this.pizzas); 
+  });
+};
 
 $(document).ready(function() {
   $("form#pizza-builder").submit(function(event) {
@@ -94,26 +99,22 @@ $(document).ready(function() {
     const inputtedSauce = $("input:radio[name='pizza-sauce']:checked").attr("id");
     const inputtedMeat = $("input:checkbox[name='pizza-meat']:checked").attr("id");
     const inputtedVeg = $("input:checkbox[name='pizza-veg']:checked").attr("id");
-    const basePrice = +0;
+    const basePrice= +0;
     let newDreamPizza = new DreamPizza(inputtedSize, inputtedCrust, inputtedSauce, inputtedMeat, inputtedVeg, basePrice);
-    displayPizzaDetails(newDreamPizza);
-    console.log(newDreamPizza.displayCostDetails); //returns undefined
-    // $("#pizza-build").append(newDreamPizza);
-
-    // let displayPizzaDetails = displayPizzaDetails(newDreamPizza);
-    // let userCost = new displayCostDetails(inputtedSize);  //this was from Brooke mtg
-    // newDreamPizza.displayPizzaDetails(newDreamPizza);
-    // userCost.displayCostDetails();    //this was from Brooke mtg //() unknown
-    // console.log(newDreamPizza.buildPizza());
-    // console.log(userCost.Cost);
-    $("#dream-pizza-show").show();
-    $('#pizza-builder').hide();
-    $('#order-instructions').hide();
+    pizzaRolodex.addPizza(newDreamPizza); //not returning at all 
+    displayPizzaDetails(pizzaRolodex); //not returning at al
+    //console.log(pizzaRolodex.pizzas); //does not return at all
+    $("#pizza-build").html(newDreamPizza); //could move to attlisteners
+    let userCost = new displayCostDetails(DreamPizza);  //this was from Brooke mtg
+    userCost.displayCostDetails();    //this was from Brooke mtg //() unknown
+    $("#dream-pizza-show").show(); //could move to attachlisteners
+    $("")
+    $('#pizza-builder').hide(); //could move to attachlisteners
+    $('#order-instructions').hide();  //could move to attachlisteners
 
     //Refresh Dream Pizza UI
-    $("#try-again").click(function() {
+    $("#try-again").click(function() {//could move to attachlisteners
       window.location.reload();
     });
   });
-
 }); // closing bracket for original doc.ready(fxn)//
