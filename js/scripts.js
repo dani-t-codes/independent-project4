a735cc1903d6b0e5bdf5b9690c4744f9df5fc158
Danielle Thompson <danithompson74@gmail.com>
2020-11-16 23:43:15 -0800

    Get fullPizzaDetails and assignBaseCost displaying to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,17 @@ function DreamPizza(size, crust, sauce, meatToppings, vegToppings, totalCost) {
   this.sauce = sauce;
   this.meatToppings = meatToppings;
   this.vegToppings = vegToppings;
-  this.totalCost = totalCost += 10;
-  //this.totalCost = [];
+  this.totalCost = 0;
 }
 
 DreamPizza.prototype.fullPizzaDetails = function() {
-  return this.size + ' pizza with ' + this.crust + " crust, " + this.sauce + ' sauce, ' + this.meatToppings + ", and " + this.vegToppings + ".";
-} 
+  return this.size + '-size pizza with ' + this.crust + " crust, " + this.sauce + ' sauce, ' + this.meatToppings + ", and " + this.vegToppings + ".";
+}; 
+
+DreamPizza.prototype.assignBaseCost = function () {
+  this.totalCost += 10;
+  return this.totalCost;
+}
 
 DreamPizza.prototype.sizeCost = function() {
   if (this.size === 'Medium') {
@@ -28,39 +32,37 @@ DreamPizza.prototype.addOnGFCost = function() {
     return this.totalCost += 5;
   }
 };
+
 //User Interface ------------
-function displayCostDetails(dreamPizzaCostToDisplay) { 
-  let pizzaCostDisplay = $("ul#pizza-cost");
-  let htmlForPizzaCost = '';
-  dreamPizzaCostToDisplay.pizzas.forEach(function(dreamPizza) {
-    htmlForPizzaCost += DreamPizza.prototype.sizeCost;
-    newDreamPizza.addOnGFCost;
-    newDreamPizza.addOnMeatToppingsCost;
-    newDreamPizza.addOnVegToppingsCost;
-  });
-  pizzaCostDisplay.html(htmlForPizzaCost); //currently not returning at all
-}
-// dreamPizza.html(this.totalCost.sizeCost.addOnGFCost.addOnToppingsCost); //as is does not work to combine all the fxns
+// function displayPizzaDetails(dreamPizzaToDisplay) {
+//   let pizzaBuildDisplay = $("ul#pizza-build");
+//   let htmlForPizzaDeets = '';
+//   dreamPizzaToDisplay.forEach(function(newDreamPizza) {
+//     htmlForPizzaDeets += "<li>" + inputtedSize + ' pizza with ' + inputtedCrust+ " crust, " + inputtedSauce + ' sauce, ' + inputtedMeat + ", " + inputtedVeg + "." + "</li>";
+//   });
+//   pizzaBuildDisplay.html(htmlForPizzaDeets);
+// }
 
-function displayPizzaDetails(dreamPizzaDisplay) {
-  let pizzaBuildDisplay = $("ul#pizza-build");
-  let htmlForPizzaDeets = '';
-  dreamPizzaDisplay.pizzas.forEach(function(newDreamPizza) {
-    htmlForPizzaDeets += "<li>" + inputtedSize + ' pizza with ' + inputtedCrust+ " crust, " + inputtedSauce + ' sauce, ' + inputtedMeat + ", " + inputtedVeg + "." + "</li>";
-  });
-  pizzaBuildDisplay.html(htmlForPizzaDeets);
-}
+// function displayCostDetails(dreamPizzaCostToDisplay) { 
+//   let pizzaCostDisplay = $("ul#pizza-cost");
+//   let htmlForPizzaCost = '';
+//   dreamPizzaCostToDisplay.forEach(function(newDreamPizza) {
+//     htmlForPizzaCost += newDreamPizza.sizeCost;
+//     newDreamPizza.addOnGFCost;
+//   });
+//   pizzaCostDisplay.html(htmlForPizzaCost);
+// }
 
-function showPizza() {
-  $("#dream-pizza-show").show();
-  $("#pizza-build").html(dreamPizzaDisplay); 
-}
+// function showPizza() {
+//   $("#dream-pizza-show").show();
+//   $("#pizza-build").html(dreamPizzaDisplay); 
+// }
 
-function attachPizzaListeners() {
-  $("ul#pizza-build").on("click", "li", function() {
-    showPizza(this.pizzas); 
-  });
-};
+// function attachPizzaListeners() {
+//   $("ul#pizza-build").on("click", "li", function() {
+//     showPizza(this.pizzas); 
+//   });
+// };
 
 $(document).ready(function() {
   $("form#pizza-builder").submit(function(event) {
@@ -70,13 +72,12 @@ $(document).ready(function() {
     const inputtedSauce = $("input:radio[name='pizza-sauce']:checked").attr("id");
     const inputtedMeat = $("input:radio[name='pizza-meat']:checked").attr("id");
     const inputtedVeg = $("input:radio[name='pizza-veg']:checked").attr("id");
-    const basePrice= +0;
-    let newDreamPizza = new DreamPizza(inputtedSize, inputtedCrust, inputtedSauce, inputtedMeat, inputtedVeg, basePrice);
-    newDreamPizza.addPizza(); //not returning at all 
-    displayPizzaDetails(newDreamPizza); //not returning at al
-    $("#pizza-build").html(newDreamPizza); //could move to attlisteners
-    let userCost = displayCostDetails(newDreamPizza);  //this was from Brooke mtg
-    userCost.displayCostDetails();    //this was from Brooke mtg //() unknown
+    let newDreamPizza = new DreamPizza(inputtedSize, inputtedCrust, inputtedSauce, inputtedMeat, inputtedVeg);
+    //let userCost = displayCostDetails(newDreamPizza);  //this was from Brooke mtg
+    //displayPizzaDetails(newDreamPizza); //not returning at all
+    $("#pizza-build").html(newDreamPizza.fullPizzaDetails()); //could move to attlisteners
+    $("#pizza-cost").html('$' + newDreamPizza.assignBaseCost());
+    //userCost.displayCostDetails();    //this was from Brooke mtg //() unknown
     $("#dream-pizza-show").show(); //could move to attachlisteners
     $('#pizza-builder').hide(); //could move to attachlisteners
     $('#order-instructions').hide();  //could move to attachlisteners
